8f8fad75e8a8693e5d260c6d762cb9a840677b71
Lucas Oliveira Garcez <estudosengcomputacao@gmail.com>
2025-11-07 14:43:21 -0300

    fix: applying spring boot configurations

--- src/test/java/com/sistemaclinico/clinico/SistemaClinicoApplicationTests.java
+++ src/test/java/com/sistemaclinico/clinico/SistemaClinicoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hospital.hospital_web;
+package com.sistemaclinico.clinico;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HospitalWebApplicationTests {
+class SistemaClinicoApplicationTests {
 
 	@Test
 	void contextLoads() {
